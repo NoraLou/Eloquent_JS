0fe36c7a2df4516ab22f6a49349dcd2166fb962d
nora humpage <nora.humpage@gmail.com>
2015-05-29 16:21:34 -0700

    changed higher-order functions

--- higher_order_fx.js
+++ higher_order_fx.js
@@ -7,17 +7,17 @@ initial try customEvery
 
 
 //answer is returned true only if everything is even
-// function customEvery(array){
-//   var answer = true
-//   array.forEach(function(i){
-//       if(i % 2 != 0){
-//         answer = false
-//         console.log(answer)
-//         return answer
-//       }
-//   })
-//   return answer
-// }
+function customEvery(array){
+  var answer = true
+  array.forEach(function(i){
+      if(i % 2 != 0){
+        answer = false
+        console.log(answer)
+        return answer
+      }
+  })
+  return answer
+}
 
 /*
 improved more abstract version customEvery
@@ -25,57 +25,57 @@ with outside function
 */
 
 
-// var test = function(i){
-//   return i % 2 == 0
-// }
+var test = function(i){
+  return i % 2 == 0
+}
 
-// var tokenArray = [2,4,6,7]
+var tokenArray = [2,4,6,7]
 
-// function customEvery(array, test){
-//   var answer = true
-//   for (var i = 0; i <= array.length-1; i++) {
-//    if (test(array[i]) == false){
-//      answer = false
-//      return answer
-//    }
-//   }
-// return answer
-// }
+function customEvery(array, test){
+  var answer = true
+  for (var i = 0; i <= array.length-1; i++) {
+   if (test(array[i]) == false){
+     answer = false
+     return answer
+   }
+  }
+return answer
+}
 
 // console.log(customEvery(tokenArray,test))
 
 //answer is returned true if at least one item meets condition.
 
 /*CUSTOM SOME*/
-//LOST ON THIS ONE TRY AGAIN
 
-// someArray = ["a", "b", "c"]
+someArray = ["a", "b", "c"]
 
-// var sometest = function(array, x){
-//   return array[i] == x
-// }
+function customSome(array, value){
+  var someAnswer = false
+  array.forEach(function(i){
+    if( i === value){
+      someAnswer = true
+      return someAnswer
+    }
+  })
+  return someAnswer
+}
+// console.log(customSome(someArray, "e"))
 
-// function customSome(array, test){
-//   for (var i = 0; i <array.length; i++) {
-//     if(test(array[i]) == true){
-//       return true
-//     }
-//   }
-//  return false
-// }
-
-// console.log(customSome(someArray, sometest("i", "a")))
 
 /*write a function to see if some
 word in an array begins
 with the letter "a"*/
 
-// function beginwithA (string){
-//  return string.slice(0,1) == "a"
+function beginwithA (string){
+ var testString = string.slice(0,1)
+ return testString == "a"
+}
 
-// }
+//console.log(beginwithA("apple"))
 
-// console.log(["banana", "boat", "cheese", "apple"].some(beginwithA()))
+
+console.log(["banana", "boat", "cheese", "apple"].some(beginwithA))
 
 
 
