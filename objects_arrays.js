69295ce7da50c4f3e7fa8f6a39a7f5c55faa2de6
nora humpage <nora.humpage@gmail.com>
2015-08-31 18:16:25 -0700

    reviewed linked lists in js

--- objects_arrays.js
+++ objects_arrays.js
@@ -1,63 +1,15 @@
-
-//My function
-// function deepEqual(valOne, valTwo){
-//   var answer = false;
-//   //if they are both values that are equal
-//   if(valOne === valTwo){
-//     answer = true
-//     return answer
-//   }
-//   if(typeof valOne === "object" && valOne!= null
-//     && typeof valTwo === "object" && valTwo != null ){
-//       console.log("pass")
-
-//       if(Object.keys(valOne).length == Object.keys(valTwo).length){
-//         console.log("passAgain")
-//           for(prop in valOne){
-//             if(!valTwo[prop]){
-//               return answer
-//             }
-//           }
-
-//           for(prop in valOne){
-//             if(valOne[prop] == valTwo[prop]){
-//               answer = true
-//               return answer
-//             }else{
-//               return answer
-//             }
-//           }
-
-//       }else{
-//         return answer
-//       }
-
-//   }else{
-//     return answer
-//   }
-
-//   return answer
-// }
-
-//WHY DOES THIS NOT WORK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// DEEP EQUAL
 
 var obj = {here: {is: "an"}, object:2, nothere: "nope!"};
 var obj2 = {here: {is: "an"}, object:2, nothere: "nope!"};
 
-console.log(deepEqual(obj, obj2));
-
-
-//Refactoring to make more sense:
+// console.log(deepEqual(obj, obj2));
 
 //check if they are values that are equal
-
 //check if they are null, or not an object
-
 //if they are an object
   //check the number of properties
-
   //check if the property has the same value
-
 
 
 function deepEqual(valOne, valTwo){
@@ -84,10 +36,59 @@ function deepEqual(valOne, valTwo){
     if(valOne[prop] != valTwo[valOne[prop]])
       return false
   }
-
-
 return true
 }
+
+
+//ARRAY TO LINKED LIST
+
+var testArg = [1,2,3]
+
+el = 10
+
+list = {value:undefined, rest:{ } }
+
+function prepend(el, list){
+  var newList = {
+    value: el,
+    rest: list
+  }
+  return newList;
+}
+
+function arrayToList(array){
+  var list = null;
+  for (var i = array.length - 1; i >= 0; i--) {
+    list = {value: array[i], rest: list};
+  }
+  return list;
+};
+
+function listToArray(list){
+  var myArray = [];
+  for (var node = list; node; node = node.rest) {
+    myArray.push(node.value);
+  }
+  return myArray;
+}
+
+// console.log(listToArray({value: 10, rest: {value: 20, rest: null}}))
+
+
+var myList = {value: 10, rest: {value: 20, rest: null}};
+
+function nth(num, list){
+  for (var node = list; node; node = node.rest){
+    if(node.value == num){
+      return node.value;
+    }
+  }
+}
+
+
+console.log(nth(arrayToList([10, 20, 30]), 1))
+
+
 
 
 
